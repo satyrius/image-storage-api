d1eba15c7d9123ca363ace4069dff7e7e979a46b
Anton Egorov <anton.egoroff@gmail.com>
2014-12-24 00:20:07 +0300

    jpeg fixture

--- storage/files/tests/test_upload.py
+++ storage/files/tests/test_upload.py
@@ -1,11 +1,15 @@
+from os import path
 from django.test import TestCase
 
 
 class UploadTest(TestCase):
     urls = 'files.urls'
 
+    def setUp(self):
+        self.filename = path.join(path.dirname(__file__), 'my_dog.jpg')
+
     def test_upload(self):
-        with open(__file__, 'r') as f:
+        with open(self.filename, 'r') as f:
             res = self.client.post('/upload', data={'file': f})
             self.assertEqual(res.status_code, 200)
             self.assertEqual(res['content-type'], 'application/json')
