2096375bcd679470edbfb81de10c723ebd3ea15f
Anton Egorov <anton.egoroff@gmail.com>
2014-12-25 21:33:59 +0300

    do not check csrf token for file upload

--- storage/files/views.py
+++ storage/files/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from .forms import UploadFileForm
 
 
 @require_POST
+@csrf_exempt
 def upload(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
